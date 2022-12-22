74bb2aa05a994934870b5b5414dbc3633ab8099e
CaioProg <caioprog18@gmail.com>
2022-12-22 20:10:18 -0300

    Crud

---
+++ MyStore/MyStore/Pages/Clients/Create.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyStore.Pages.Clients
+{
+    public class CreateModel : PageModel
+    {
+        public ClientInfo clientInfo = new ClientInfo();
+        public String errorMessage = "";
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            clientInfo.name = Request.Form["name"];
+            clientInfo.email = Request.Form["email"];
+            clientInfo.phone = Request.Form["phone"];
+            clientInfo.address = Request.Form["address"];
+
+            if (clientInfo.name.Length == 0 || clientInfo.email.Length == 0 || clientInfo.phone.Length == 0 || clientInfo.address.Length == 0)
+            {
+                errorMessage = "All the fields are required";
+                return;
+            }
+        }
+    }
+}
